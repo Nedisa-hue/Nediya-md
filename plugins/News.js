dae03b5705c80885ab0d91c6e079f02a3d5c375e
Nedisa-hue <Nediyaboy@gmail.com>
2025-02-26 14:33:43 +0530

    Create News.js

---
+++ plugins/News.js
@@ -0,0 +1,57 @@
+const axios = require('axios');
+const { cmd } = require('../command');
+const { fetchJson } = require('../lib/functions');
+//This Plugin Created By Suhas Bro ➠ Chethiya_MD_00
+//Don't Sell This Plugin in Enyone.This Plugin is Give Free.
+//Subscribe me on YouTube ➠ @suhasbro
+//https://www.youtube.com/@suhasbro
+
+
+
+cmd({
+    pattern: "hirucheck",
+    alias: ["hirunews","newshiru","hirulk"],
+    react: "⭐",
+    category: "search hiru news",
+    desc: "Fetch the latest news from the SUHAS API in Hiru API.",
+    use: "",
+    filename: __filename,
+},
+    async (conn, mek, m, {
+        from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber,
+        botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName,
+        participants, groupAdmins, isBotAdmins, isAdmins, reply
+    }) => {
+        try {
+            const apiUrl = `https://suhas-bro-apii.vercel.app/hiru`;
+//Dont Change This API Key
+            const response = await axios.get(apiUrl);
+            const data = response.data;
+
+            if (!data || !data.newsURL || !data.title || !data.image || !data.text) {
+                return reply(`*No News Available At This Moment* ❗`);
+            }
+
+            const { newsURL, title, image, text, Power } = data;
+
+            let newsInfo = "chethiya_MD_00 𝐍𝐞𝐰𝐬 𝐔𝐩𝐝𝐚𝐭𝐞 📰\n\n";
+            newsInfo += `✨ *Title*: ${title}\n\n`;
+            newsInfo += `📑 *Description*:\n${text}\n\n`;
+            newsInfo += `⛓️‍💥 *Url*: www.hirunews.lk\n\n`;
+            newsInfo += `> *© 𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝙱𝚢 chethiya_MD_00  〽️𝙳*\n\n*${Power}*`;
+
+            if (image) {
+                await conn.sendMessage(m.chat, {
+                    image: { url: image },
+                    caption: newsInfo,
+                }, { quoted: m });
+            } else {
+                await conn.sendMessage(m.chat, { text: newsInfo }, { quoted: m });
+            }
+
+        } catch (error) {
+            console.error(error);
+            reply(`*An Error Occurred While Fetching News At This Moment* ❗`);
+        }
+    }
+);
